47ef92a360e15d8a7f85945cc979dcfbeea7f30a
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-06-24 16:17:28 +0200

    header file

--- sort.h
+++ sort.h
@@ -68,10 +68,9 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-int partition(int *array, int low, int high);
-void quick_sort_recursive(int *array, int low, int high);
 void quick_sort(int *array, size_t size);
-
+void quick_sort_recursive(int *array, int low, int high);
+int partition(int *array, int low, int high);
 /* ---- Advanced ---- */
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
