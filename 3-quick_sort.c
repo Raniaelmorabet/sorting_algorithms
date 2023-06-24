b287a4c3562bae09f5f2b56064c4d3b90c1003c6
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-06-24 16:10:46 +0200

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -3,57 +3,55 @@
 /**
  * partition - array partition
  * @array: array to sort
- * @first: first position
- * @last: last position
- * @size: array size
+ * @low: lowest index
+ * @high: highest index
  * Return: int pivot index
  */
-int partition(int *array, int first, int last, size_t size)
+int partition(int *array, int low, int high)
 {
-	int i = first - 1, aux, j;
+    int pivot = array[high];
+    int i = (low - 1);
 
-	for (j = first; j <= last - 1; j++)
-	{
-		if (array[j] < array[last])
-		{
-			i++;
-			if (i < j)
-			{
-				aux = array[i];
-				array[i] = array[j];
-				array[j] = aux;
-				print_array(array, size);
-			}
-		}
-	}
-	if (array[i + 1] > array[last])
-	{
-		aux = array[i + 1];
-		array[i + 1] = array[last];
-		array[last] = aux;
-		print_array(array, size);
-	}
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (array[j] < pivot)
+        {
+            i++;
+            if (i != j)
+            {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                print_array(array, high + 1);
+            }
+        }
+    }
 
-	return (i + 1);
+    if (array[i + 1] > array[high])
+    {
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+        print_array(array, high + 1);
+    }
+
+    return (i + 1);
 }
 
 /**
- * qs - sorts an array of integers recursively
+ * quick_sort_recursive - sorts an array of integers recursively
  * @array: array to sort
- * @first: first position
- * @last: last position
- * @size: array size
+ * @low: lowest index
+ * @high: highest index
  */
-void qs(int *array, int first, int last, size_t size)
+void quick_sort_recursive(int *array, int low, int high)
 {
-	int pivot;
-
-	if (first < last)
-	{
-		pivot = partition(array, first, last, size);
-		qs(array, first, pivot - 1, size);
-		qs(array, pivot + 1, last, size);
-	}
+    if (low < high)
+    {
+        int pivot = partition(array, low, high);
+        quick_sort_recursive(array, low, pivot - 1);
+        quick_sort_recursive(array, pivot + 1, high);
+    }
 }
 
 /**
@@ -64,5 +62,8 @@ void qs(int *array, int first, int last, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-	qs(array, 0, size - 1, size);
+    if (array == NULL || size < 2)
+        return;
+
+    quick_sort_recursive(array, 0, size - 1);
 }
