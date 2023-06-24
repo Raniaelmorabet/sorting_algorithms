9a0f565a58b5f0dbc38c3fac4813165f6312b65d
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-06-24 13:07:25 +0100

    add: all prototypes

--- sort.h
+++ sort.h
@@ -18,14 +18,67 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * enum kind_e - Kind of card
+ *
+ * @SPADE: Spade
+ * @HEART: Heart
+ * @CLUB: Club
+ * @DIAMOND: Diamond
+ */
+typedef enum kind_e
+{
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
+} kind_t;
+
+/**
+ * struct card_s - Playing card
+ *
+ * @value: Value of the card
+ * From "Ace" to "King"
+ * @kind: Kind of the card
+ */
+typedef struct card_s
+{
+	const char *value;
+	const kind_t kind;
+} card_t;
+
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+typedef struct deck_node_s
+{
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
+} deck_node_t;
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-
-/* 0. Bubble sort */
+/* ---- Mandatory ---- */
 void bubble_sort(int *array, size_t size);
-
-/* 1. Insertion sort */
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+/* ---- Advanced ---- */
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
 
 #endif /* SORT_H */
