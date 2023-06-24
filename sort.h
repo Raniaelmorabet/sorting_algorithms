6d6118e7987dd9062c6fc859e0ef901027c9eb75
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-06-24 15:59:26 +0200

    header file

--- sort.h
+++ sort.h
@@ -69,9 +69,9 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void quicksort(int *array, int lo, int hi)
-int lomuto_partition(int *array, int lo, int hi)
-
+void swap(int *a, int *b);
+int lomuto_partition(int *array, int low, int high);
+void quicksort(int *array, int low, int high);
 /* ---- Advanced ---- */
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
