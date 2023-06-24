a4d58bcfca22b23bab96c48c02f973489b43153e
Raniaelmorabet <raniaelmorabet9@gmail.com>
2023-06-24 06:24:42 -0700

    header file

--- sort.h
+++ sort.h
@@ -13,9 +13,9 @@
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
 /**
@@ -28,10 +28,10 @@ typedef struct listint_s
  */
 typedef enum kind_e
 {
-	SPADE = 0,
-	HEART,
-	CLUB,
-	DIAMOND
+SPADE = 0,
+HEART,
+CLUB,
+DIAMOND
 } kind_t;
 
 /**
@@ -43,8 +43,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-	const char *value;
-	const kind_t kind;
+const char *value;
+const kind_t kind;
 } card_t;
 
 /**
@@ -56,9 +56,9 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-	const card_t *card;
-	struct deck_node_s *prev;
-	struct deck_node_s *next;
+const card_t *card;
+struct deck_node_s *prev;
+struct deck_node_s *next;
 } deck_node_t;
 
 void print_array(const int *array, size_t size);
