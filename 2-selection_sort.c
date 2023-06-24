57f228e557d10a0da5f9f58ad7752127c19e9495
Raniaelmorabet <raniaelmorabet9@gmail.com>
2023-06-24 05:32:46 -0700

    a function that sorts an array of integers in ascending order using the Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts an array of integers in ascending order
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ */
+void selection_sort(int *array, size_t size)
+{
+    size_t i, j, min_idx;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        min_idx = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min_idx])
+                min_idx = j;
+        }
+        if (min_idx != i)
+        {
+            int temp = array[i];
+            array[i] = array[min_idx];
+            array[min_idx] = temp;
+            print_array(array, size);
+        }
+    }
+}
