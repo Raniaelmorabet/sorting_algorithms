e1f37bc59c35a5eda8fc87b3494f8fbd448678c6
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-06-25 08:37:01 +0100

    mv: deck struct to deck.h

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,5 +1,9 @@
 #include "sort.h"
 
+void merge_sort_helper(int *array, int left, int right);
+void merge(int *array, int p, int q, int r);
+
+
 /**
  * merge_sort - sorts an array of integers in ascending order
  * using the Merge sort algorithm
@@ -9,39 +13,106 @@
  */
 void merge_sort(int *array, size_t size)
 {
-	int *left, *right;
-	size_t i, middle;
-
 	if (!array || size < 2)
 		return;
 
-	middle = size / 2;
-	left = array;
-	right = array + middle;
+	merge_sort_helper(array, 0, size - 1);
+}
 
-	merge_sort(left, middle);
-	merge_sort(right, size - middle);
+/**
+ * merge_sort_helper - sorts an array of integers in ascending order
+ * using the Merge sort algorithm
+ * @array: array of integers
+ * @left: left index
+ * @right: right index
+ */
+void merge_sort_helper(int *array, int left, int right)
+{
+	int middle;
 
-	printf("Merging...\n");
+	if (left < right)
+	{
+		middle = (left + right) / 2;
+
+		merge_sort_helper(array, left, middle);
+		merge_sort_helper(array, middle + 1, right);
+
+		printf("Merging...\n");
+
+		merge(array, left, middle, right);
+	}
+}
+
+
+/**
+ * merge - merges two arrays
+ * @array: array of integers
+ * @p: left index
+ * @q: middle index
+ * @r: right index
+ * Return: void
+ */
+void merge(int *array, int p, int q, int r)
+{
+	int i, j, k;
+	int n1 = q - p + 1;
+	int n2 = r - q;
+	int *left, *right;
+
+	left = malloc(sizeof(int) * n1);
+	if (!left)
+		return;
+	right = malloc(sizeof(int) * n2);
+	if (!right)
+		return;
 
 	printf("[left]: ");
-	print_array(left, middle);
+	for (i = 0; i < n1; i++)
+		left[i] = array[p + i];
+	print_array(left, n1);
 
 	printf("[right]: ");
-	print_array(right, size - middle);
+	for (j = 0; j < n2; j++)
+		right[j] = array[q + 1 + j];
+	print_array(right, n2);
 
-	for (i = 0; i < middle; i++)
+	i = 0;
+	j = 0;
+	k = p;
+
+	while (i < n1 && j < n2)
 	{
-		while (*left > *right && right < array + size)
+		if (left[i] <= right[j])
 		{
-			*left = *left ^ *right;
-			*right = *left ^ *right;
-			*left = *left ^ *right;
-			left++;
-			right++;
+			array[k] = left[i];
+			i++;
 		}
+		else
+		{
+			array[k] = right[j];
+			j++;
+		}
+
+		k++;
+	}
+
+	while (i < n1)
+	{
+		array[k] = left[i];
+		i++;
+		k++;
+	}
+
+	while (j < n2)
+	{
+		array[k] = right[j];
+		j++;
+		k++;
 	}
 
 	printf("[Done]: ");
-	print_array(array, size);
+	print_array(array + p, r - p + 1);
+
+	free(left);
+	free(right);
 }
--- Makefile
+++ Makefile
@@ -9,7 +9,31 @@ all: 0
 1:
 	$(CC) $(FLAGS) 1-insertion_sort_list.c tests/1-main.c tests/print_list.c -o insertion
 
+100:
+	$(CC) $(FLAGS) 100-shell_sort.c tests/100-main.c tests/print_array.c -o shell
+
+101:
+	$(CC) $(FLAGS) 101-cocktail_sort_list.c tests/101-main.c tests/print_list.c -o cocktail
+
+102:
+	$(CC) $(FLAGS) 102-counting_sort.c tests/102-main.c tests/print_array.c -o counting
+
+103:
+	$(CC) $(FLAGS) 103-merge_sort.c tests/103-main.c tests/print_array.c -o merge
+
+104:
+	$(CC) $(FLAGS) 104-heap_sort.c tests/104-main.c tests/print_array.c -o heap
+
+105:
+	$(CC) $(FLAGS) 105-radix_sort.c tests/105-main.c tests/print_array.c -o radix
+
+106:
+	$(CC) $(FLAGS) 106-bitonic_sort.c tests/106-main.c tests/print_array.c -o bitonic
+
+107:
+	$(CC) $(FLAGS) 107-quick_sort.c tests/107-main.c tests/print_array.c -o quick
+
 clean:
-	rm -f bubble insertion
+	rm -f bubble insertion shell cocktail counting merge heap radix bitonic quick
 
 re: clean all
