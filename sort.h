736a05c7ecde01abad4790f32edd5be9e9e8e592
Raniaelmorabet <raniaelmorabet9@gmail.com>
2023-06-25 07:38:56 -0700

    header file

--- sort.h
+++ sort.h
@@ -38,5 +38,6 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
 
 #endif /* SORT_H */
