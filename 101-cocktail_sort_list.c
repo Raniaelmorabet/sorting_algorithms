b78ce378fd1ac5987b5fd51a2e550fef565af35a
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-06-25 08:42:50 +0100

    add: cocktail sort

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * cocktail_sort_list - sorts a doubly linked list of integers in ascending
+ * order using the Cocktail shaker sort algorithm
+ * @list: doubly linked list of integers
+ * Return: void
+ */
+void cocktail_sort_list(listint_t **list)
+{
+}
