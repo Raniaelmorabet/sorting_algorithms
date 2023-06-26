28e4542a46cb9c73fb1f0aff100ea8da00781623
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-06-26 13:03:25 +0200

    add :1000-sort_deck.c

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,35 +1,45 @@
-#include <stdlib.h>
-#include "deck.h"
+#include<stdlib.h>
+#include<string.h>
+#include"deck.h"
 
 /**
- * sort_deck - Sorts a deck of cards in ascending order
- * @deck: Pointer to the head of the deck
+ * compare_cards - Compare two card pointers for sorting.
+ * @card1: Pointer to the first card.
+ * @card2: Pointer to the second card.
+ * Return: Negative value if card1 should come before card2
+ */
+int compare_cards(const void *card1, const void *card2)
+{
+    const card_t *c1 = *(const card_t **)card1;
+    const card_t *c2 = *(const card_t **)card2;
+
+    int value_cmp = strcmp(c1->value, c2->value);
+    if (value_cmp != 0)
+        return value_cmp;
+
+    return c1->kind - c2->kind;
+}
+
+/**
+ * sort_deck - Sort a deck of cards in-place.
+ * @deck: Pointer to the head of the deck.
  */
 void sort_deck(deck_node_t **deck)
 {
-	deck_node_t *node_array[52];
-	deck_node_t *current = *deck;
-	size_t i = 0;
+    size_t i, count = 0;
+    deck_node_t *node = *deck;
+    card_t *cards[52];
 
-	while (current != NULL)
-	{
-		node_array[i] = current;
-		current = current->next;
-		i++;
-	}
+    while (node != NULL) {
+        cards[count++] = (card_t *)node->card;
+        node = node->next;
+    }
 
-	qsort(node_array, 52, sizeof(deck_node_t *), card_compare);
+    qsort(cards, count, sizeof(card_t *), compare_cards);
 
-	*deck = node_array[0];
-	current = *deck;
-	current->prev = NULL;
-
-	for (i = 1; i < 52; i++)
-	{
-		current->next = node_array[i];
-		node_array[i]->prev = current;
-		current = current->next;
-	}
-
-	current->next = NULL;
+    node = *deck;
+    for (i = 0; i < count; i++) {
+        node->card = cards[i];
+        node = node->next;
+    }
 }
