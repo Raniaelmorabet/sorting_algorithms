3c72e8c444664ba3852cda7e3508e12f175a59fb
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-06-26 13:38:36 +0200

    Update 1000-sort_deck.c

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,6 +1,6 @@
-#include<stdlib.h>
-#include<string.h>
-#include"deck.h"
+#include <stdlib.h>
+#include <string.h>
+#include "deck.h"
 
 /**
  * compare_cards - Compare two card pointers for sorting.
@@ -13,15 +13,18 @@ int compare_cards(const void *card1, const void *card2)
     const card_t *c1 = *(const card_t **)card1;
     const card_t *c2 = *(const card_t **)card2;
     const char *values[] = {"Jack", "4", "3", "Queen", "5", "10", "6", "9", "7", "King", "8", "2", "Ace"};
-    
+
     int index1 = -1, index2 = -1;
-    for (int i = 0; i < 13; i++) {
+    int i;
+
+    for (i = 0; i < 13; i++) {
         if (strcmp(c1->value, values[i]) == 0) {
             index1 = i;
             break;
         }
     }
-    for (int i = 0; i < 13; i++) {
+
+    for (i = 0; i < 13; i++) {
         if (strcmp(c2->value, values[i]) == 0) {
             index2 = i;
             break;
@@ -40,22 +43,22 @@ int compare_cards(const void *card1, const void *card2)
  */
 void sort_deck(deck_node_t **deck)
 {
-	size_t i, count = 0;
-	deck_node_t *node = *deck;
-	card_t *cards[52];
+    size_t i, count = 0;
+    deck_node_t *node = *deck;
+    card_t *cards[52];
 
-	while (node != NULL)
-	{
-		cards[count++] = (card_t *)node->card;
-		node = node->next;
-	}
+    while (node != NULL)
+    {
+        cards[count++] = (card_t *)node->card;
+        node = node->next;
+    }
 
-	qsort(cards, count, sizeof(card_t *), compare_cards);
+    qsort(cards, count, sizeof(card_t *), compare_cards);
 
-	node = *deck;
-	for (i = 0; i < count; i++)
-	{
-		node->card = cards[i];
-		node = node->next;
-	}
+    node = *deck;
+    for (i = 0; i < count; i++)
+    {
+        node->card = cards[i];
+        node = node->next;
+    }
 }
